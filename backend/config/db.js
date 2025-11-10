4378a2ec1f56edc9129b995a8c298d65b8154ea6
shreyasi-30 <shreyasi.gidmare.btech2023@sitnagpur.siu.edu.in>
2025-11-10 19:38:54 +0530

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,13 @@
-a 
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB Connected Successfully');
+  } catch (error) {
+    console.error('MongoDB Connection Error:', error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
